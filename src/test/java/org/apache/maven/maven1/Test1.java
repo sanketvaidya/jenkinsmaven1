da753f2d95c8f079c3cc9271c28b34d90ad695a5
Sanket Vaidya <sanket.vaidya@infostretch.com>
2019-03-05 12:50:37 +0530

    First Maven

---
+++ src/test/java/org/apache/maven/maven1/Test1.java
@@ -0,0 +1,23 @@
+package org.apache.maven.maven1;
+
+
+public class Test1 {
+
+
+	public void test1()
+	{
+        System.out.println( "This is test1!" );
+	}
+	
+
+	public void test2()
+	{
+        System.out.println( "This is test2!" );
+	}
+	
+
+	public void test3()
+	{
+        System.out.println( "This is test3!" );
+	}
+}
